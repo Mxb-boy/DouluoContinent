fa112a873385aa3f622a6a0b28e69360c678f0b5
Mxb <17688641401@163.com>
2026-06-11 17:59:27 +0800

    添加初始武器 唐刀

--- Script/Blueprint/UGCGameMode.lua
+++ Script/Blueprint/UGCGameMode.lua
@@ -1,16 +1,33 @@
 ---@class UGCGameMode_C:BP_UGCGameBase_C
 --Edit Below--
-local UGCGameMode = {}; 
+local UGCGameMode = {};
 
 function UGCGameMode:ReceiveBeginPlay()
-   
-
     UGCGenericMessageSystem.ListenGlobalMessage(
         self,
         UGCGenericMessageSystem.Messages.UGC.PlayerPawn.PawnDefeat,
         self,
         self.OnPawnDefeat
     )
+end
+
+-- 玩家登录时发初始武器（Pawn可能还没好，等1秒）
+function UGCGameMode:UGC_PlayerLoginEvent(PlayerController)
+    local PC = PlayerController
+    UGCTimerUtility.CreateLuaTimer(1, function()
+        if PC.Pawn then
+            UGCBackPackSystem.AddItem(PC.Pawn, 831602104, 1)
+            --UGCBackPackSystem.AddItem(PC.Pawn, 301001, 90)
+        end
+    end, false)
+end
+
+-- 玩家重生时发武器
+function UGCGameMode:UGC_PlayerRespawnEvent(RespawnedController)
+    if RespawnedController.Pawn then
+        UGCBackPackSystem.AddItem(RespawnedController.Pawn, 831602104, 1)
+        --UGCBackPackSystem.AddItem(RespawnedController.Pawn, 301001, 90)
+    end
 end
 
 function UGCGameMode:OnPawnDefeat(VictimPlayerKey, InstigatorPlayerKey, DamageType)
