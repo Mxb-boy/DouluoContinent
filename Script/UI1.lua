ef7f94262544dda3627213294d86428ebb3fe110
WIN-GTQHON1SP1Q\Administrator <3231283647@qq.com>
2026-06-13 15:40:46 +0800

    制作部分UI

---
+++ Script/UI1.lua
@@ -0,0 +1,25 @@
+---@class UI1_C:UUserWidget
+---@field Button_60 UButton
+---@field Button_1015 UButton
+---@field Image_37 UImage
+---@field Image_59 UImage
+---@field Image_198 UImage
+---@field ProgressBar_187 UProgressBar
+--Edit Below--
+local UI1 = { bInitDoOnce = false } 
+
+--[==[ Construct
+function UI1:Construct()
+	
+end
+-- Construct ]==]
+
+-- function UI1:Tick(MyGeometry, InDeltaTime)
+
+-- end
+
+-- function UI1:Destruct()
+
+-- end
+
+return UI1
---
+++ Script/UI2t.lua
@@ -0,0 +1,21 @@
+---@class UI2t_C:UUserWidget
+---@field Image_225 UImage
+---@field ProgressBar_122 UProgressBar
+--Edit Below--
+local UI2t = { bInitDoOnce = false } 
+
+--[==[ Construct
+function UI2t:Construct()
+	
+end
+-- Construct ]==]
+
+-- function UI2t:Tick(MyGeometry, InDeltaTime)
+
+-- end
+
+-- function UI2t:Destruct()
+
+-- end
+
+return UI2t
