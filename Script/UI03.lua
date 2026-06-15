81070fc217c0954f90538c90b9a42483b17e908d
WIN-GTQHON1SP1Q\Administrator <3231283647@qq.com>
2026-06-15 16:54:23 +0800

    提交UI

---
+++ Script/UI03.lua
@@ -0,0 +1,25 @@
+---@class UI03_C:UUserWidget
+---@field Image_31 UImage
+---@field Image_56 UImage
+---@field Image_106 UImage
+---@field Image_252 UImage
+---@field Image_348 UImage
+---@field Image_501 UImage
+--Edit Below--
+local UI03 = { bInitDoOnce = false } 
+
+--[==[ Construct
+function UI03:Construct()
+	
+end
+-- Construct ]==]
+
+-- function UI03:Tick(MyGeometry, InDeltaTime)
+
+-- end
+
+-- function UI03:Destruct()
+
+-- end
+
+return UI03
---
+++ Script/UI04.lua
@@ -0,0 +1,76 @@
+---@class UI04_C:UUserWidget
+---@field Button_620 UButton
+---@field Button_621 UButton
+---@field Button_622 UButton
+---@field Button_623 UButton
+---@field Button_624 UButton
+---@field Button_625 UButton
+---@field Button_626 UButton
+---@field Button_627 UButton
+---@field Button_628 UButton
+---@field Button_629 UButton
+---@field Button_630 UButton
+---@field Button_631 UButton
+---@field Button_632 UButton
+---@field Button_633 UButton
+---@field Image_34 UImage
+---@field Image_81 UImage
+---@field Image_110 UImage
+---@field Image_1391 UImage
+---@field Image_1483 UImage
+---@field Image_1489 UImage
+---@field Image_1490 UImage
+---@field Image_1491 UImage
+---@field Image_1492 UImage
+---@field Image_1493 UImage
+---@field Image_1494 UImage
+---@field Image_1495 UImage
+---@field Image_1496 UImage
+---@field Image_1497 UImage
+---@field Image_1498 UImage
+---@field Image_1499 UImage
+---@field Image_1500 UImage
+---@field Image_1501 UImage
+---@field Image_1502 UImage
+---@field Image_1503 UImage
+---@field Image_1504 UImage
+---@field Image_1505 UImage
+---@field Image_1506 UImage
+---@field Image_1507 UImage
+---@field Image_1508 UImage
+---@field Image_1509 UImage
+---@field Image_1510 UImage
+---@field Image_1511 UImage
+---@field Image_1512 UImage
+---@field Image_1513 UImage
+---@field Image_1514 UImage
+---@field Image_1515 UImage
+---@field Image_1516 UImage
+---@field Image_1517 UImage
+---@field Image_1518 UImage
+---@field Image_1519 UImage
+---@field Image_1520 UImage
+---@field Image_1521 UImage
+---@field Image_1522 UImage
+---@field Image_1523 UImage
+---@field Image_1524 UImage
+---@field Image_1525 UImage
+---@field Image_1595 UImage
+--Edit Below--
+local UI04 = { bInitDoOnce = false } 
+
+--[==[ Construct
+function UI04:Construct()
+	
+end
+-- Construct ]==]
+
+-- function UI04:Tick(MyGeometry, InDeltaTime)
+
+-- end
+
+-- function UI04:Destruct()
+
+-- end
+
+return UI04
