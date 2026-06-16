04e512f5722ad0a1e541740cc060c22c84ea9322
陈思妍 <3231283647@qq.com>
2026-06-16 21:41:11 +0800

    称号

---
+++ Script/UI06.lua
@@ -0,0 +1,57 @@
+---@class UI06_C:UUserWidget
+---@field Button_0 UButton
+---@field Button_1 UButton
+---@field Button_2 UButton
+---@field Button_3 UButton
+---@field Button_4 UButton
+---@field Button_5 UButton
+---@field Button_6 UButton
+---@field Button_7 UButton
+---@field Button_8 UButton
+---@field Button_15 UButton
+---@field Button_16 UButton
+---@field Button_17 UButton
+---@field Button_18 UButton
+---@field Button_19 UButton
+---@field Button_209 UButton
+---@field Button_333 UButton
+---@field Button_345 UButton
+---@field Image_0 UImage
+---@field Image_1 UImage
+---@field Image_2 UImage
+---@field Image_3 UImage
+---@field Image_4 UImage
+---@field Image_5 UImage
+---@field Image_6 UImage
+---@field Image_7 UImage
+---@field Image_8 UImage
+---@field Image_15 UImage
+---@field Image_16 UImage
+---@field Image_17 UImage
+---@field Image_18 UImage
+---@field Image_19 UImage
+---@field Image_26 UImage
+---@field Image_93 UImage
+---@field Image_97 UImage
+---@field Image_98 UImage
+---@field Image_99 UImage
+---@field Image_123 UImage
+---@field Image_132 UImage
+--Edit Below--
+local UI06 = { bInitDoOnce = false } 
+
+--[==[ Construct
+function UI06:Construct()
+	
+end
+-- Construct ]==]
+
+-- function UI06:Tick(MyGeometry, InDeltaTime)
+
+-- end
+
+-- function UI06:Destruct()
+
+-- end
+
+return UI06
