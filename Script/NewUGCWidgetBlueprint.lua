eb8d02423d6e55f0fefcbe8f6fd2125f8f193aff
陈思妍 <3231283647@qq.com>
2026-06-22 17:26:41 +0800

    UI10

---
+++ Script/NewUGCWidgetBlueprint.lua
@@ -0,0 +1,21 @@
+---@class NewUGCWidgetBlueprint_C:UUserWidget
+---@field Button_97 UButton
+---@field Image_55 UImage
+--Edit Below--
+local NewUGCWidgetBlueprint = { bInitDoOnce = false } 
+
+--[==[ Construct
+function NewUGCWidgetBlueprint:Construct()
+	
+end
+-- Construct ]==]
+
+-- function NewUGCWidgetBlueprint:Tick(MyGeometry, InDeltaTime)
+
+-- end
+
+-- function NewUGCWidgetBlueprint:Destruct()
+
+-- end
+
+return NewUGCWidgetBlueprint
---
+++ Script/UI10.lua
@@ -0,0 +1,39 @@
+---@class UI10_C:UUserWidget
+---@field Button_78 UButton
+---@field Button_125 UButton
+---@field Image_66 UImage
+---@field Image_112 UImage
+---@field Image_180 UImage
+---@field Image_181 UImage
+---@field Image_297 UImage
+---@field Image_299 UImage
+---@field Image_814 UImage
+---@field Image_982 UImage
+---@field Image_983 UImage
+---@field Image_984 UImage
+---@field Image_985 UImage
+---@field Image_986 UImage
+---@field NewUGCWidgetBlueprint_C_0 NewUGCWidgetBlueprint_C
+---@field NewUGCWidgetBlueprint_C_1 NewUGCWidgetBlueprint_C
+---@field NewUGCWidgetBlueprint_C_2 NewUGCWidgetBlueprint_C
+---@field NewUGCWidgetBlueprint_C_3 NewUGCWidgetBlueprint_C
+---@field NewUGCWidgetBlueprint_C_4 NewUGCWidgetBlueprint_C
+---@field NewUGCWidgetBlueprint_C_5 NewUGCWidgetBlueprint_C
+--Edit Below--
+local UI10 = { bInitDoOnce = false } 
+
+--[==[ Construct
+function UI10:Construct()
+	
+end
+-- Construct ]==]
+
+-- function UI10:Tick(MyGeometry, InDeltaTime)
+
+-- end
+
+-- function UI10:Destruct()
+
+-- end
+
+return UI10
