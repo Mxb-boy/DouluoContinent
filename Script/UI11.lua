4d666ac2fb0333305c3e937471342cf24096e6fc
陈思妍 <3231283647@qq.com>
2026-06-24 12:06:59 +0800

    UI11

---
+++ Script/UI11.lua
@@ -0,0 +1,40 @@
+---@class UI11_C:UUserWidget
+---@field Button_89 UButton
+---@field Button_90 UButton
+---@field Button_92 UButton
+---@field Button_93 UButton
+---@field Button_94 UButton
+---@field Button_95 UButton
+---@field Button_118 UButton
+---@field Image_32 UImage
+---@field Image_41 UImage
+---@field Image_83 UImage
+---@field Image_84 UImage
+---@field NewUGCWidgetBlueprint1 NewUGCWidgetBlueprint1_C
+---@field NewUGCWidgetBlueprint1_C_195 NewUGCWidgetBlueprint1_C
+---@field NewUGCWidgetBlueprint1_C_196 NewUGCWidgetBlueprint1_C
+---@field NewUGCWidgetBlueprint1_C_197 NewUGCWidgetBlueprint1_C
+---@field NewUGCWidgetBlueprint1_C_198 NewUGCWidgetBlueprint1_C
+---@field NewUGCWidgetBlueprint1_C_199 NewUGCWidgetBlueprint1_C
+---@field REINST_NewUGCWidgetBlueprint1_C_0 NewUGCWidgetBlueprint1_C
+---@field REINST_NewUGCWidgetBlueprint1_C_1 NewUGCWidgetBlueprint1_C
+---@field REINST_NewUGCWidgetBlueprint1_C_3 NewUGCWidgetBlueprint1_C
+---@field REINST_NewUGCWidgetBlueprint1_C_4 NewUGCWidgetBlueprint1_C
+--Edit Below--
+local UI11 = { bInitDoOnce = false } 
+
+--[==[ Construct
+function UI11:Construct()
+	
+end
+-- Construct ]==]
+
+-- function UI11:Tick(MyGeometry, InDeltaTime)
+
+-- end
+
+-- function UI11:Destruct()
+
+-- end
+
+return UI11
---
+++ Script/Widget01.lua
@@ -0,0 +1,37 @@
+---@class Widget01_C:UUserWidget
+---@field Button_0 UButton
+---@field Button_1 UButton
+---@field Button_129 UButton
+---@field Image_0 UImage
+---@field Image_1 UImage
+---@field Image_2 UImage
+---@field Image_3 UImage
+---@field Image_4 UImage
+---@field Image_5 UImage
+---@field Image_6 UImage
+---@field Image_7 UImage
+---@field Image_8 UImage
+---@field Image_54 UImage
+---@field Image_145 UImage
+---@field Image_146 UImage
+---@field Image_147 UImage
+---@field Image_148 UImage
+---@field Image_149 UImage
+--Edit Below--
+local NewUGCWidgetBlueprint1 = { bInitDoOnce = false } 
+
+--[==[ Construct
+function NewUGCWidgetBlueprint1:Construct()
+	
+end
+-- Construct ]==]
+
+-- function NewUGCWidgetBlueprint1:Tick(MyGeometry, InDeltaTime)
+
+-- end
+
+-- function NewUGCWidgetBlueprint1:Destruct()
+
+-- end
+
+return NewUGCWidgetBlueprint1
