b0ee07c2670a022df36cc85aa95f6370aea6358c
陈思妍 <3231283647@qq.com>
2026-06-26 18:27:50 +0800

    UI13

--- Script/UI13.lua
+++ Script/UI13.lua
@@ -1,7 +1,18 @@
 ---@class UI13_C:UUserWidget
+---@field Button_106 UButton
 ---@field Image_0 UImage
 ---@field Image_1 UImage
+---@field Image_29 UImage
+---@field Image_30 UImage
 ---@field Image_42 UImage
+---@field NewUGCWidgetBlueprint1 NewUGCWidgetBlueprint1_C
+---@field NewUGCWidgetBlueprint1_C_0 NewUGCWidgetBlueprint1_C
+---@field NewUGCWidgetBlueprint1_C_1 NewUGCWidgetBlueprint1_C
+---@field NewUGCWidgetBlueprint1_C_2 NewUGCWidgetBlueprint1_C
+---@field NewUGCWidgetBlueprint1_C_3 NewUGCWidgetBlueprint1_C
+---@field NewUGCWidgetBlueprint1_C_4 NewUGCWidgetBlueprint1_C
+---@field NewUGCWidgetBlueprint1_C_5 NewUGCWidgetBlueprint1_C
+---@field NewUGCWidgetBlueprint1_C_6 NewUGCWidgetBlueprint1_C
 --Edit Below--
 local UI13 = { bInitDoOnce = false } 
 
