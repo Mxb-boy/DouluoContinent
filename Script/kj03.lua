d8eee4c1cbae869962662085e09dbeb2bdeac493
陈思妍 <3231283647@qq.com>
2026-06-27 17:27:08 +0800

    kj03-商品弹窗

---
+++ Script/kj03.lua
@@ -0,0 +1,29 @@
+---@class kj03_C:UUserWidget
+---@field Button_73 UButton
+---@field Button_128 UButton
+---@field Image_71 UImage
+---@field Image_142 UImage
+---@field Image_203 UImage
+---@field Image_204 UImage
+---@field Image_299 UImage
+---@field Image_444 UImage
+---@field Image_445 UImage
+---@field Image_502 UImage
+--Edit Below--
+local kj03 = { bInitDoOnce = false } 
+
+--[==[ Construct
+function kj03:Construct()
+	
+end
+-- Construct ]==]
+
+-- function kj03:Tick(MyGeometry, InDeltaTime)
+
+-- end
+
+-- function kj03:Destruct()
+
+-- end
+
+return kj03
