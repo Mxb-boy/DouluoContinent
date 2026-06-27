b37800ed0b255076d3311f0678903f3a3807bed8
陈思妍 <3231283647@qq.com>
2026-06-27 16:29:51 +0800

    修改UI

---
+++ Script/kj01.lua
@@ -0,0 +1,44 @@
+---@class kj01_C:UUserWidget
+---@field Button_58 UButton
+---@field Image_0 UImage
+---@field Image_46 UImage
+---@field Image_47 UImage
+---@field Image_48 UImage
+---@field Image_49 UImage
+---@field Image_50 UImage
+---@field Image_51 UImage
+---@field Image_52 UImage
+---@field Image_53 UImage
+---@field Image_54 UImage
+---@field Image_55 UImage
+---@field Image_56 UImage
+---@field Image_57 UImage
+---@field Image_58 UImage
+---@field Image_59 UImage
+---@field Image_60 UImage
+---@field Image_61 UImage
+---@field Image_62 UImage
+---@field Image_63 UImage
+---@field Image_64 UImage
+---@field Image_65 UImage
+---@field Image_100 UImage
+---@field Image_101 UImage
+---@field Image_134 UImage
+--Edit Below--
+local kj01 = { bInitDoOnce = false } 
+
+--[==[ Construct
+function kj01:Construct()
+	
+end
+-- Construct ]==]
+
+-- function kj01:Tick(MyGeometry, InDeltaTime)
+
+-- end
+
+-- function kj01:Destruct()
+
+-- end
+
+return kj01
