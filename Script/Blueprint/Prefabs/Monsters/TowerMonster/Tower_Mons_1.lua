8dc0ba503ebef554ea320d990f00f6669676e3e0
林君朋 <doudouqie66@gmail.com>
2026-07-03 23:59:28 +0800

    添加塔的怪物

---
+++ Script/Blueprint/Prefabs/Monsters/TowerMonster/Tower_Mons_1.lua
@@ -0,0 +1,171 @@
+---@class Tower_Mons_1_C:BP_UGC_GenericMobPawn_Base_C
+---@field InBox UBoxComponent
+---@field OutBox UBoxComponent
+---@field HitBox UCapsuleComponent
+---@field TowerLevelID int32
+--Edit Below--
+local Tower_Mons_1 = {}
+
+local function DisableMonsterCollision(monster)
+    if monster.HitBox ~= nil then
+        monster.HitBox:SetCollisionEnabled(ECollisionEnabled.NoCollision)
+    end
+
+    if monster.StaticMesh ~= nil then
+        monster.StaticMesh:SetCollisionEnabled(ECollisionEnabled.NoCollision)
+    end
+end
+-- function Tower_Mons_1:ReceiveBeginPlay()
+--     Tower_Mons_1.SuperClass.ReceiveBeginPlay(self)
+-- end
+
+-- function Tower_Mons_1:ReceiveTick(DeltaTime)
+--     Tower_Mons_1.SuperClass.ReceiveTick(self, DeltaTime)
+-- end
+
+-- function Tower_Mons_1:ReceiveEndPlay()
+--     Tower_Mons_1.SuperClass.ReceiveEndPlay(self) 
+-- end
+
+-- function Tower_Mons_1:GetReplicatedProperties()
+--     return
+-- end
+
+-- ---受击前置事件
+-- ---生效范围：服务器
+-- ---@param Damage float 伤害值
+-- ---@param EventInstigator AController 伤害来源的Controller
+-- ---@param DamageCauser AActor 伤害来源
+-- ---@param DamageContext FGameMagnitudeContext  伤害上下文
+-- function Tower_Mons_1:PreTakeDamageEvent(Damage, EventInstigator, DamageCauser, DamageContext)
+     
+-- end
+
+-- ---受击后置事件
+-- ---生效范围：服务器
+-- ---@param Damage float 伤害值
+-- ---@param EventInstigator AController 伤害来源的Controller
+-- ---@param DamageCauser AActor 伤害来源
+-- ---@param DamageContext FGameMagnitudeContext  伤害上下文
+-- function Tower_Mons_1:PostTakeDamageEvent(Damage, EventInstigator, DamageCauser, DamageContext)
+    
+-- end
+
+-- ---受击前置伤害修改
+-- ---生效范围：服务器
+-- ---@param Damage float 伤害值
+-- ---@param EventInstigator AController 伤害来源的Controller
+-- ---@param DamageCauser AActor 伤害来源
+-- ---@param DamageContext FGameMagnitudeContext  伤害上下文
+-- ---@return float 修改后的伤害值
+-- function Tower_Mons_1:PreOverrideDamage(Damage, EventInstigator, DamageCauser, DamageContext)
+--     return Damage
+-- end
+
+-- ---受击后置伤害修改
+-- ---生效范围：服务器
+-- ---@param Damage float 伤害值
+-- ---@param EventInstigator AController 伤害来源的Controller
+-- ---@param DamageCauser AActor 伤害来源
+-- ---@param DamageContext FGameMagnitudeContext  伤害上下文
+-- ---@return float 修改后的伤害值
+-- function Tower_Mons_1:PostOverrideDamage(Damage, EventInstigator, DamageCauser, DamageContext)
+--     return Damage
+-- end
+
+---角色死亡事件
+---生效范围：服务器&客户端
+---@param Damage float 伤害值
+---@param EventInstigator AController 伤害来源的Controller
+---@param DamageCauser AActor 伤害来源
+---@param FDamageEvent DamageEvent 伤害事件
+---@param DamageTypeID int32 伤害类型
+function Tower_Mons_1:BPDie(KillingDamage, EventInstigator, DamageCauser, DamageEvent, DamageTypeID)
+    DisableMonsterCollision(self)
+
+    if self:HasAuthority() and self.SpawnWall ~= nil then
+        self.SpawnWall:OnMonsterDied(self)
+    end
+
+    if self:HasAuthority() then
+        local DropID = self.MonsterID
+        if EventInstigator ~= nil and EventInstigator.PlayerState ~= nil then
+            local Probability_Bonus = EventInstigator.PlayerState.Probability_Bonus or 0
+            if Probability_Bonus > 100 then
+                Probability_Bonus = 100
+            end
+            DropID = Probability_Bonus * 100 + self.MonsterID
+        end
+
+        -- 只有服务端才可以掉落
+        if DropID ~= nil then
+            self.UGCPresetCommonDropItemComponent:StartDropByProduceID(
+                DropID,
+                -1,
+                EUGCGenerateItemEntityType.GenerateItemEntity_WrapperActor,
+                nil
+            )
+        end
+    end
+end
+
+-- ---状态进入事件
+-- ---生效范围：服务器&客户端
+-- ---@param DynamicState FGameplayTag 进入的状态
+-- function Tower_Mons_1:OnEnterTagState_BP(DynamicState)
+--     local Tag = BlueprintGameplayTagLibrary.GetTagName(DynamicState)
+--     ugcprint('OnEnterTagState_BP: ' .. Tag)
+-- end
+
+-- ---状态退出事件
+-- ---生效范围：服务器&客户端
+-- ---@param DynamicState FGameplayTag 退出的状态
+-- function Tower_Mons_1:OnLeaveTagState_BP(DynamicState)
+--     local Tag = BlueprintGameplayTagLibrary.GetTagName(DynamicState)
+--     ugcprint('OnLeaveTagState_BP: ' .. Tag)
+-- end
+
+-- ---状态打断事件
+-- ---生效范围：服务器&客户端
+-- ---@param DynamicState FGameplayTag 打断的状态
+-- function Tower_Mons_1:OnInterruptTagState_BP(DynamicState)
+--     local Tag = BlueprintGameplayTagLibrary.GetTagName(DynamicState)
+--     ugcprint('OnInterruptTagState_BP' .. Tag)
+-- end
+
+-- ---行为树消息
+-- ---生效范围：服务器
+-- ---@param NotifyMsg string 消息
+-- function Tower_Mons_1:OnBehaviorNotify_BP(NotifyMsg)
+--     ugcprint('OnBehaviorNotify_BP: ' .. NotifyMsg)
+-- end
+
+-- ---怪物的目标发生变化事件
+-- ---生效范围：服务器&客户端
+-- ---@param NewTarget AActor 新目标
+-- ---@param OldTarget AActor 旧目标
+-- function Tower_Mons_1:OnTargetChange_BP(NewTarget, OldTarget)
+    
+-- end
+
+-- [Editor Generated Lua] function define Begin:
+function Tower_Mons_1:LuaInit()
+	if self.bInitDoOnce then
+		return;
+	end
+	self.bInitDoOnce = true;
+	-- [Editor Generated Lua] BindingProperty Begin:
+	-- [Editor Generated Lua] BindingProperty End;
+	
+	-- [Editor Generated Lua] BindingEvent Begin:
+	self.OutBox.OnComponentHit:Add(self.OutBox_OnComponentHit, self);
+	-- [Editor Generated Lua] BindingEvent End;
+end
+
+function Tower_Mons_1:OutBox_OnComponentHit(HitComponent, OtherActor, OtherComp, NormalImpulse, Hit)
+	return nil;
+end
+
+-- [Editor Generated Lua] function define End;
+
+return Tower_Mons_1
---
+++ Script/Blueprint/Prefabs/Monsters/TowerMonster/Tower_Mons_4.lua
@@ -0,0 +1,116 @@
+---@class Tower_Mons_4_C:Tower_Mons_1_C
+--Edit Below--
+local Tower_Mons_4 = {}
+
+-- function Tower_Mons_4:ReceiveBeginPlay()
+--     Tower_Mons_4.SuperClass.ReceiveBeginPlay(self)
+-- end
+
+-- function Tower_Mons_4:ReceiveTick(DeltaTime)
+--     Tower_Mons_4.SuperClass.ReceiveTick(self, DeltaTime)
+-- end
+
+-- function Tower_Mons_4:ReceiveEndPlay()
+--     Tower_Mons_4.SuperClass.ReceiveEndPlay(self) 
+-- end
+
+-- function Tower_Mons_4:GetReplicatedProperties()
+--     return
+-- end
+
+-- ---受击前置事件
+-- ---生效范围：服务器
+-- ---@param Damage float 伤害值
+-- ---@param EventInstigator AController 伤害来源的Controller
+-- ---@param DamageCauser AActor 伤害来源
+-- ---@param DamageContext FGameMagnitudeContext  伤害上下文
+-- function Tower_Mons_4:PreTakeDamageEvent(Damage, EventInstigator, DamageCauser, DamageContext)
+     
+-- end
+
+-- ---受击后置事件
+-- ---生效范围：服务器
+-- ---@param Damage float 伤害值
+-- ---@param EventInstigator AController 伤害来源的Controller
+-- ---@param DamageCauser AActor 伤害来源
+-- ---@param DamageContext FGameMagnitudeContext  伤害上下文
+-- function Tower_Mons_4:PostTakeDamageEvent(Damage, EventInstigator, DamageCauser, DamageContext)
+    
+-- end
+
+-- ---受击前置伤害修改
+-- ---生效范围：服务器
+-- ---@param Damage float 伤害值
+-- ---@param EventInstigator AController 伤害来源的Controller
+-- ---@param DamageCauser AActor 伤害来源
+-- ---@param DamageContext FGameMagnitudeContext  伤害上下文
+-- ---@return float 修改后的伤害值
+-- function Tower_Mons_4:PreOverrideDamage(Damage, EventInstigator, DamageCauser, DamageContext)
+--     return Damage
+-- end
+
+-- ---受击后置伤害修改
+-- ---生效范围：服务器
+-- ---@param Damage float 伤害值
+-- ---@param EventInstigator AController 伤害来源的Controller
+-- ---@param DamageCauser AActor 伤害来源
+-- ---@param DamageContext FGameMagnitudeContext  伤害上下文
+-- ---@return float 修改后的伤害值
+-- function Tower_Mons_4:PostOverrideDamage(Damage, EventInstigator, DamageCauser, DamageContext)
+--     return Damage
+-- end
+
+---角色死亡事件
+---生效范围：服务器&客户端
+---@param Damage float 伤害值
+---@param EventInstigator AController 伤害来源的Controller
+---@param DamageCauser AActor 伤害来源
+---@param FDamageEvent DamageEvent 伤害事件
+---@param DamageTypeID int32 伤害类型
+function Tower_Mons_4:BPDie(KillingDamage, EventInstigator, DamageCauser, DamageEvent, DamageTypeID)
+    if self:HasAuthority() then
+        -- 只有服务端才可以掉落
+        self.UGCPresetCommonDropItemComponent:StartDrop(self, EventInstigator, {})
+    end
+end
+
+-- ---状态进入事件
+-- ---生效范围：服务器&客户端
+-- ---@param DynamicState FGameplayTag 进入的状态
+-- function Tower_Mons_4:OnEnterTagState_BP(DynamicState)
+--     local Tag = BlueprintGameplayTagLibrary.GetTagName(DynamicState)
+--     ugcprint('OnEnterTagState_BP: ' .. Tag)
+-- end
+
+-- ---状态退出事件
+-- ---生效范围：服务器&客户端
+-- ---@param DynamicState FGameplayTag 退出的状态
+-- function Tower_Mons_4:OnLeaveTagState_BP(DynamicState)
+--     local Tag = BlueprintGameplayTagLibrary.GetTagName(DynamicState)
+--     ugcprint('OnLeaveTagState_BP: ' .. Tag)
+-- end
+
+-- ---状态打断事件
+-- ---生效范围：服务器&客户端
+-- ---@param DynamicState FGameplayTag 打断的状态
+-- function Tower_Mons_4:OnInterruptTagState_BP(DynamicState)
+--     local Tag = BlueprintGameplayTagLibrary.GetTagName(DynamicState)
+--     ugcprint('OnInterruptTagState_BP' .. Tag)
+-- end
+
+-- ---行为树消息
+-- ---生效范围：服务器
+-- ---@param NotifyMsg string 消息
+-- function Tower_Mons_4:OnBehaviorNotify_BP(NotifyMsg)
+--     ugcprint('OnBehaviorNotify_BP: ' .. NotifyMsg)
+-- end
+
+-- ---怪物的目标发生变化事件
+-- ---生效范围：服务器&客户端
+-- ---@param NewTarget AActor 新目标
+-- ---@param OldTarget AActor 旧目标
+-- function Tower_Mons_4:OnTargetChange_BP(NewTarget, OldTarget)
+    
+-- end
+
+return Tower_Mons_4
