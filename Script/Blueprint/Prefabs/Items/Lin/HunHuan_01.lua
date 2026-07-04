1a8899b44affaf9ce75cd8ba74ad4ba4e040f535
Mxb <17688641401@163.com>
2026-07-04 15:00:06 +0800

    Revert "去掉物品可删除"
    This reverts commit f6d8f7eec4dc8f958469602e0c65076ef23c8551.

--- Script/Blueprint/Prefabs/Items/Lin/HunHuan_01.lua
+++ Script/Blueprint/Prefabs/Items/Lin/HunHuan_01.lua
@@ -1,6 +1,4 @@
 ---@class HunHuan_01_C:UGCItemHandle_ConsumeBase_C
---Edit Below--
----@class HunHuan_01_C:UGCItemHandle_ConsumeBase_C
 -- Edit Below--
 local HunHuan_01 = {}
 local L_Com = UGCGameSystem.UGCRequire("Script.Lin.L_Com")
