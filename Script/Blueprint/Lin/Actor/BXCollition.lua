643e63ea00a1dad7ef1105e9a850c0531c0b68a8
10 <481982877@qq.com>
2026-07-04 14:44:15 +0800

    修改场景

---
+++ Script/Blueprint/Lin/Actor/BXCollition.lua
@@ -0,0 +1,45 @@
+---@class BXCollition_C:AActor
+---@field Box UBoxComponent
+---@field DefaultSceneRoot USceneComponent
+---@field TowerID int32
+--Edit Below--
+local BXCollition = {}
+function  BXCollition:ReceiveBeginPlay()
+    self.SuperClass.ReceiveBeginPlay(self);
+	self:LuaInit();
+    if self:HasAuthority() then
+	self.Box.OnComponentBeginOverlap:Add(self.Box_OnComponentBeginOverlap, self);
+        
+    end
+end 
+
+function BXCollition:Box_OnComponentBeginOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult)
+    local pc = OtherActor:GetPlayerControllerSafety()
+      if pc then
+          pc:Server_TeleportToSpawn(self.CSPoint)
+      end
+end
+
+
+
+-- [Editor Generated Lua] function define Begin:
+function BXCollition:LuaInit()
+	if self.bInitDoOnce then
+		return;
+	end
+	self.bInitDoOnce = true;
+	-- [Editor Generated Lua] BindingProperty Begin:
+	-- [Editor Generated Lua] BindingProperty End;
+	
+	-- [Editor Generated Lua] BindingEvent Begin:
+	self.Box.OnComponentEndOverlap:Add(self.Box_OnComponentEndOverlap, self);
+	-- [Editor Generated Lua] BindingEvent End;
+end
+
+function BXCollition:Box_OnComponentEndOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex)
+	return nil;
+end
+
+-- [Editor Generated Lua] function define End;
+
+return BXCollition
