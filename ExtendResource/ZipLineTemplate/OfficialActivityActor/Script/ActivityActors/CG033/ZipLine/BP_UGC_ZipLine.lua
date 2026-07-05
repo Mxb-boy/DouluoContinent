fd86bb40568631010c7c44a57ce560c4197e1ff6
10 <481982877@qq.com>
2026-07-05 20:56:24 +0800

    绳索

---
+++ ExtendResource/ZipLineTemplate/OfficialActivityActor/Script/ActivityActors/CG033/ZipLine/BP_UGC_ZipLine.lua
@@ -0,0 +1,131 @@
+---@class BP_UGC_ZipLine_C:ActivityBaseActor
+---@field ChildAttachScene USceneComponent
+---@field Sphere USphereComponent
+---@field ClickActorComponentBase UClickActorComponentBase
+---@field AttachScene USceneComponent
+---@field Cube1 UStaticMeshComponent
+---@field DefaultSceneRoot USceneComponent
+---@field TargetZipLine BP_UGC_ZipLine_C
+---@field Rope BP_UGC_Rope_C
+---@field RopeClass UClass
+---@field ChildClass UClass
+---@field ProhibitState ULuaArrayHelper<EPawnState>
+---@field DeattachAreaRadius float
+--Edit Below--
+local BP_UGC_ZipLine = {
+    bSpawn = false,
+    bCD = false,
+}
+ 
+function BP_UGC_ZipLine:PrintVector(Vector)
+    print("BP_UGC_ZipLine:PrintVector(Vector)--X = "..tostring(Vector.X).." Y = "..tostring(Vector.Y).." Z = "..tostring(Vector.Z))
+end
+function BP_UGC_ZipLine:ReceiveBeginPlay()
+    BP_UGC_ZipLine.SuperClass.ReceiveBeginPlay(self)
+    if not UGCObjectUtility.IsObjectValid(self.TargetZipLine) then
+        return
+    end
+    if self.TargetZipLine.TargetZipLine ~= self then
+        UGCTimerUtility.CreateLuaTimer(0.5, function()
+            self.Rope = UGCActorComponentUtility.SpawnActor(self, self.RopeClass, self.AttachScene:K2_GetComponentLocation(), self.AttachScene:K2_GetComponentRotation(), Vector.New(1, 1, 1), self)
+            self.Rope.EndMesh:K2_SetWorldLocation(self.TargetZipLine.AttachScene:K2_GetComponentLocation())
+        end,false)
+    else
+        print("BP_UGC_ZipLine:ReceiveBeginPlay--spawn")
+        UGCTimerUtility.CreateLuaTimer(0.5, function()
+            self.Rope = UGCActorComponentUtility.SpawnActor(self, self.RopeClass, self.AttachScene:K2_GetComponentLocation(), self.AttachScene:K2_GetComponentRotation(), Vector.New(1, 1, 1), self)
+            self:PrintVector(self.Cube1:K2_GetComponentLocation())
+            --self.TargetZipLine.Rope = self.Rope
+            self.Rope.EndMesh:K2_SetWorldLocation(self.TargetZipLine.AttachScene:K2_GetComponentLocation())
+        end,false)
+    end
+end
+function BP_UGC_ZipLine:CanClickZipLineUI(ClickParams)
+    if not UGCObjectUtility.IsObjectValid(self.TargetZipLine) then
+        return false
+    end
+    local CheckActor = ClickParams.PlayerController:GetPlayerCharacterSafety()
+    for _, State in pairs(self.ProhibitState) do
+        if CheckActor:HasState(State) then
+            return false
+        end
+    end
+    return true
+end
+function BP_UGC_ZipLine:OnClickedZipLineUI(ClickParams)
+    print("BP_UGC_ZipLine:OnClickedZipLineUI(ClickParams)")
+    if not UGCGameSystem.IsServer() then
+        return
+    end
+    if self.bCD then
+        return
+    else
+        Timer.InsertTimer(0.5,function ()
+            self.bCD = false
+        end,false)
+    end
+    self.bCD = true
+    local EndLocation = self.TargetZipLine.AttachScene:K2_GetComponentLocation()--UGCMathUtility.AddVector(self.TargetZipLine.AttachScene:K2_GetComponentLocation(), UGCMathUtility.MultiplyVector(Direct, 88))
+    local Direct = UGCMathUtility.GetDirectionUnitVector(self.AttachScene:K2_GetComponentLocation(), EndLocation)
+    local DeattachLocation = UGCMathUtility.AddVector(EndLocation, UGCMathUtility.MultiplyVector(Direct, -150))
+    local StartLocation = UGCMathUtility.AddVector(self.AttachScene:K2_GetComponentLocation(), UGCMathUtility.MultiplyVector(Direct, 88))
+    local RotationFind_StartLocation = self.ChildAttachScene:K2_GetComponentLocation()
+    local RotationFind_EndLocation = self.TargetZipLine.ChildAttachScene:K2_GetComponentLocation()
+    local Rotation = KismetMathLibrary.FindLookAtRotation(RotationFind_StartLocation,RotationFind_EndLocation)
+    Rotation.Pitch = 0
+    Rotation.Roll = 0
+    StartLocation.Z = StartLocation.Z - 160
+    DeattachLocation.Z = DeattachLocation.Z - 160
+    local ZipLineChild = UGCActorComponentUtility.SpawnActor(self, self.ChildClass, StartLocation,Rotation, Vector.New(1, 1, 1), self)
+    ZipLineChild.OwnerZipLine = self
+    ZipLineChild.TargetZipLine = self.TargetZipLine
+    ZipLineChild:PossessWithAttach(ClickParams.PlayerController,StartLocation,DeattachLocation)
+end
+
+--]]
+
+--[[
+function BP_UGC_ZipLine:ReceiveTick(DeltaTime)
+    BP_UGC_ZipLine.SuperClass.ReceiveTick(self, DeltaTime)
+end
+--]]
+
+--[[
+function BP_UGC_ZipLine:ReceiveEndPlay()
+    BP_UGC_ZipLine.SuperClass.ReceiveEndPlay(self) 
+end
+--]]
+
+--[[
+function BP_UGC_ZipLine:GetReplicatedProperties()
+    return
+end
+--]]
+
+--[[
+function BP_UGC_ZipLine:GetAvailableServerRPCs()
+    return
+end
+--]]
+
+-- [Editor Generated Lua] function define Begin:
+function BP_UGC_ZipLine:LuaInit()
+	if self.bInitDoOnce then
+		return;
+	end
+	self.bInitDoOnce = true;
+	-- [Editor Generated Lua] BindingProperty Begin:
+	-- [Editor Generated Lua] BindingProperty End;
+	
+	-- [Editor Generated Lua] BindingEvent Begin:
+	self.ChildAttachScene.TransformUpdatedDynamic:Add(self.ChildAttachScene_TransformUpdatedDynamic, self);
+	-- [Editor Generated Lua] BindingEvent End;
+end
+
+function BP_UGC_ZipLine:ChildAttachScene_TransformUpdatedDynamic()
+	return nil;
+end
+
+-- [Editor Generated Lua] function define End;
+
+return BP_UGC_ZipLine
---
+++ ExtendResource/ZipLineTemplate/OfficialActivityActor/Script/ActivityActors/CG033/ZipLine/BP_UGC_ZipLineChild.lua
@@ -0,0 +1,133 @@
+---@class BP_UGC_ZipLineChild_C:ActivityBaseActor
+---@field Slide UStaticMeshComponent
+---@field ActorSequence UActorSequenceComponent
+---@field ActivityFakePossess UActivityFakePossessComponent
+---@field SkeletalMesh USkeletalMeshComponent
+---@field Scene USceneComponent
+---@field CustomActorMove UCustomActorMoveComponent
+---@field DefaultSceneRoot USceneComponent
+---@field ClimbSpeed float
+---@field ActiveSeqBind FActivityActorSequenceBinding
+---@field DisableState ULuaArrayHelper<EPawnState>
+---@field RopeClass UClass
+--Edit Below--
+local BP_UGC_ZipLineChild = {
+    OwnerZipLine = nil,
+    TargetZipLine = nil,
+}
+
+function BP_UGC_ZipLineChild:AreaBlockadeDetect(BeginLocation,EndLocation)
+    local IgnoreActors = {self}
+
+    if UGCObjectUtility.IsObjectValid(self.OwnerZipLine) then
+        table.insert(IgnoreActors, self.OwnerZipLine)
+    end
+
+    if UGCObjectUtility.IsObjectValid(self.TargetZipLine) then
+        table.insert(IgnoreActors, self.TargetZipLine)
+    end
+
+    if UGCObjectUtility.IsObjectValid(self.PlayerController) then
+        local PlayerCharacter = self.PlayerController:GetPlayerCharacterSafety()
+        if UGCObjectUtility.IsObjectValid(PlayerCharacter) then
+            table.insert(IgnoreActors, PlayerCharacter)
+        end
+    end
+
+    local bHit,HitResult = 
+    KismetSystemLibrary.SphereTraceSingle(self, 
+    BeginLocation,
+    EndLocation,
+    40, 
+    ECollisionChannel.ECC_WorldDynamic, 
+    false,
+    IgnoreActors
+    )
+    if not bHit then
+        print_dev("BP_UGC_ZipLineChild:AreaBlockadeDetect--true")
+        return false
+    end
+    if UGCObjectUtility.IsA(HitResult.Actor:Get(),self.RopeClass) then
+        return false
+    end
+    print_dev("BP_UGC_ZipLineChild:AreaBlockadeDetect--false--HitResult.Actor:Get() = "..KismetSystemLibrary.GetDisplayName(HitResult.Actor:Get()))
+    return true
+end
+
+function BP_UGC_ZipLineChild:PossessWithAttach(PC,StartLocation,EndLocation)
+    print("BP_UGC_ZipLineChild:PossessWithAttach")
+    self.ActivityFakePossess:FakePossessWithAttach(PC,self.Scene,"None")
+    self.PlayerController = PC
+    self.CustomActorMove:SetPosition(StartLocation, EndLocation)
+    --print("BP_LadderChild:OnClickUpUI--ClimbSpeed = "..tostring(self.ClimbSpeed))
+    self.CustomActorMove:SetMoveSpeed(self.ClimbSpeed)
+    self.CustomActorMove:StartMove()
+    self:JumpToState("Active")
+    if UGCGameSystem.IsServer() then
+        for _, State in ipairs(self.DisableState) do
+            UGCPawnSystem.DisabledPawnState(self.PlayerController:GetPlayerCharacterSafety(), State, true)
+        end
+    end
+end
+function BP_UGC_ZipLineChild:ActivityFakePossess_OnUnPossess(PC)
+    print("BP_UGC_Ladder:OnUnPossess")
+    UGCTimerUtility.RemoveLuaTimerByName("CheckBlockTimer")
+    UGCTimerUtility.CreateLuaTimer(0.1, function()
+        self:K2_DestroyActor()
+    end,false)
+end
+
+function BP_UGC_ZipLineChild:OnPlayerAttachedToThisActor_BP(InPlayer)
+    print("BP_LadderChild:OnPlayerAttachedToThisActor_BP")
+    self.ActorSequence:AddBinding(self.ActiveSeqBind.Binding, InPlayer, false)
+end
+
+function BP_UGC_ZipLineChild:ReceiveBeginPlay()
+    BP_UGC_ZipLineChild.SuperClass.ReceiveBeginPlay(self)
+    self.CustomActorMove.ActorMoveEvent:Add(self.CustomActorMove_ActorMoveEvent, self);
+    self.ActivityFakePossess.OnUnPossess:Add(self.ActivityFakePossess_OnUnPossess, self);
+    UGCTimerUtility.CreateLuaTimer(0.2, function()
+        local bBlock = self:AreaBlockadeDetect(self.Scene:K2_GetComponentLocation(),self.SkeletalMesh:K2_GetComponentLocation())
+        print_dev("BP_UGC_ZipLineChild:ReceiveBeginPlay--bBlock = "..tostring(bBlock))
+        if bBlock then
+            self.ActivityFakePossess:FakeUnPossessWithDettach(self.PlayerController,EUnPossessReason.Finished)
+        end
+    end,true,"CheckBlockTimer")
+end
+--]]
+
+--[[
+function BP_UGC_ZipLineChild:ReceiveTick(DeltaTime)
+    BP_UGC_ZipLineChild.SuperClass.ReceiveTick(self, DeltaTime)
+end
+--]]
+
+--[[
+function BP_UGC_ZipLineChild:ReceiveEndPlay()
+    BP_UGC_ZipLineChild.SuperClass.ReceiveEndPlay(self) 
+end
+--]]
+
+--[[
+function BP_UGC_ZipLineChild:GetReplicatedProperties()
+    return
+end
+--]]
+
+--[[
+function BP_UGC_ZipLineChild:GetAvailableServerRPCs()
+    return
+end
+--]]
+
+-- [Editor Generated Lua] function define Begin:
+function BP_UGC_ZipLineChild:CustomActorMove_ActorMoveEvent(bIsMove)
+    if not bIsMove then
+        self.ActivityFakePossess:FakeUnPossessWithDettach(self.PlayerController,EUnPossessReason.Finished)
+    end
+	return nil;
+end
+
+-- [Editor Generated Lua] function define End;
+
+return BP_UGC_ZipLineChild
