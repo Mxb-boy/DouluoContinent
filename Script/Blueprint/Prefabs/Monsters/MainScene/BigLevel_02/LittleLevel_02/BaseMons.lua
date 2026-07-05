ddf334e71dff0182b48815cf4b99a26a27c0ed9d
林君朋 <doudouqie66@gmail.com>
2026-07-05 12:38:28 +0800

    修改怪物

--- Script/Blueprint/Prefabs/Monsters/MainScene/BigLevel_02/LittleLevel_02/BaseMons.lua
+++ Script/Blueprint/Prefabs/Monsters/MainScene/BigLevel_02/LittleLevel_02/BaseMons.lua
@@ -1,5 +1,4 @@
----@class BaseMons_C:BP_UGC_GenericStaticMeshMob_C
----@field StaticMesh UStaticMeshComponent
+---@class BaseMons_C:BP_UGC_GenericMobPawn_Base_C
 ---@field HitBox UCapsuleComponent
 ---@field MonsterID int32
 --Edit Below--
