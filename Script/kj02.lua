7921ca1fd691e349963341445f96aae913bbb4b3
陈思妍 <3231283647@qq.com>
2026-07-10 15:45:11 +0800

    首充礼包

---
+++ Script/kj02.lua
@@ -0,0 +1,29 @@
+---@class kj02_C:UUserWidget
+---@field Button_108 UButton
+---@field Button_237 UButton
+---@field Image_45 UImage
+---@field Image_107 UImage
+---@field Image_108 UImage
+---@field Image_109 UImage
+---@field Image_110 UImage
+---@field Image_111 UImage
+---@field Image_112 UImage
+---@field Image_113 UImage
+--Edit Below--
+local kj02 = { bInitDoOnce = false } 
+
+--[==[ Construct
+function kj02:Construct()
+	
+end
+-- Construct ]==]
+
+-- function kj02:Tick(MyGeometry, InDeltaTime)
+
+-- end
+
+-- function kj02:Destruct()
+
+-- end
+
+return kj02
---
+++ Script/kj04.lua
@@ -0,0 +1,29 @@
+---@class kj02_C:UUserWidget
+---@field Button_108 UButton
+---@field Button_237 UButton
+---@field Image_45 UImage
+---@field Image_107 UImage
+---@field Image_108 UImage
+---@field Image_109 UImage
+---@field Image_110 UImage
+---@field Image_111 UImage
+---@field Image_112 UImage
+---@field Image_113 UImage
+--Edit Below--
+local kj04 = { bInitDoOnce = false } 
+
+--[==[ Construct
+function kj04:Construct()
+	
+end
+-- Construct ]==]
+
+-- function kj04:Tick(MyGeometry, InDeltaTime)
+
+-- end
+
+-- function kj04:Destruct()
+
+-- end
+
+return kj04
