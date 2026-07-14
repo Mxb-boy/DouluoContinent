d9cfa3364bee031074f56449c0c4e7b97f7d0be5
陈思妍 <3231283647@qq.com>
2026-07-14 18:23:16 +0800

    组队UI

---
+++ Script/UI015.lua
@@ -0,0 +1,24 @@
+---@class UI015_C:UUserWidget
+---@field Button_71 UButton
+---@field Image_31 UImage
+---@field Image_32 UImage
+---@field Image_33 UImage
+---@field kj05 kj05_C
+--Edit Below--
+local UI015 = { bInitDoOnce = false } 
+
+--[==[ Construct
+function UI015:Construct()
+	
+end
+-- Construct ]==]
+
+-- function UI015:Tick(MyGeometry, InDeltaTime)
+
+-- end
+
+-- function UI015:Destruct()
+
+-- end
+
+return UI015
---
+++ Script/kj05.lua
@@ -0,0 +1,25 @@
+---@class kj05_C:UUserWidget
+---@field Button_1 UButton
+---@field Button_3 UButton
+---@field Button_108 UButton
+---@field Image_45 UImage
+---@field Image_108 UImage
+---@field Image_113 UImage
+--Edit Below--
+local kj05 = { bInitDoOnce = false } 
+
+--[==[ Construct
+function kj05:Construct()
+	
+end
+-- Construct ]==]
+
+-- function kj05:Tick(MyGeometry, InDeltaTime)
+
+-- end
+
+-- function kj05:Destruct()
+
+-- end
+
+return kj05
