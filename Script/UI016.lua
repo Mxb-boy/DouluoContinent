e33674968057357f384aeb304cca87e21c599d34
陈思妍 <3231283647@qq.com>
2026-07-16 10:09:33 +0800

    添加爬塔和图鉴UI

---
+++ Script/UI016.lua
@@ -0,0 +1,68 @@
+---@class UI016_C:UUserWidget
+---@field Button_0 UButton
+---@field Button_71 UButton
+---@field Button_100 UButton
+---@field Button_105 UButton
+---@field Button_106 UButton
+---@field Button_107 UButton
+---@field Button_108 UButton
+---@field Button_109 UButton
+---@field Button_110 UButton
+---@field Button_111 UButton
+---@field Button_112 UButton
+---@field Button_113 UButton
+---@field Button_114 UButton
+---@field Button_115 UButton
+---@field Image_0 UImage
+---@field Image_1 UImage
+---@field Image_2 UImage
+---@field Image_3 UImage
+---@field Image_31 UImage
+---@field Image_32 UImage
+---@field Image_33 UImage
+---@field Image_181 UImage
+---@field Image_190 UImage
+---@field Image_200 UImage
+---@field Image_201 UImage
+---@field Image_202 UImage
+---@field Image_203 UImage
+---@field Image_204 UImage
+---@field Image_205 UImage
+---@field Image_206 UImage
+---@field Image_207 UImage
+---@field Image_208 UImage
+---@field Image_209 UImage
+---@field Image_210 UImage
+---@field Image_211 UImage
+---@field Image_212 UImage
+---@field Image_213 UImage
+---@field Image_214 UImage
+---@field Image_215 UImage
+---@field Image_216 UImage
+---@field Image_217 UImage
+---@field Image_218 UImage
+---@field Image_219 UImage
+---@field Image_220 UImage
+---@field Image_221 UImage
+---@field Image_269 UImage
+---@field Image_270 UImage
+---@field Image_271 UImage
+---@field Image_272 UImage
+--Edit Below--
+local UI016 = { bInitDoOnce = false } 
+
+--[==[ Construct
+function UI016:Construct()
+	
+end
+-- Construct ]==]
+
+-- function UI016:Tick(MyGeometry, InDeltaTime)
+
+-- end
+
+-- function UI016:Destruct()
+
+-- end
+
+return UI016
