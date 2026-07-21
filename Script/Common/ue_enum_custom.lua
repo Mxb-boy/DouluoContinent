2e4c1782953eea9f1fe0a6694b20f8554a31280a
Mxb <17688641401@163.com>
2026-07-21 18:35:17 +0800

    改错了

--- Script/Common/ue_enum_custom.lua
+++ Script/Common/ue_enum_custom.lua
@@ -33,14 +33,6 @@ EGiftPackType = {
 }; 
 
 
----@enum ESignInEventType
-ESignInEventType = { 
-    Monthly = 0,
-    Weekly = 1,
-    OneOff = 2,
-}; 
-
-
 ---@enum ELotteryResetType
 ELotteryResetType = { 
     NotReset = 0,
