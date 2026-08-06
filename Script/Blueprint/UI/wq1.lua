22c326521fdac2a9b6a0cbe8d503567b38ba4f80
Mxb <17688641401@163.com>
2026-08-06 12:28:45 +0800

    UI21 wq1 wq2  新功能UI

---
+++ Script/Blueprint/UI/wq1.lua
@@ -0,0 +1,22 @@
+---@class wq1_C:UUserWidget
+---@field Btn_LeftSlot_1 UButton
+---@field Img_LeftLong_1 UImage
+---@field Img_LeftSelected_1 UImage
+--Edit Below--
+local wq1 = { bInitDoOnce = false } 
+
+--[==[ Construct
+function wq1:Construct()
+	
+end
+-- Construct ]==]
+
+-- function wq1:Tick(MyGeometry, InDeltaTime)
+
+-- end
+
+-- function wq1:Destruct()
+
+-- end
+
+return wq1
---
+++ Script/Blueprint/UI/wq2.lua
@@ -0,0 +1,21 @@
+---@class wq2_C:UUserWidget
+---@field Btn_RightSlot_1 UButton
+---@field Img_RightSlot_1 UImage
+--Edit Below--
+local wq2 = { bInitDoOnce = false } 
+
+--[==[ Construct
+function wq2:Construct()
+	
+end
+-- Construct ]==]
+
+-- function wq2:Tick(MyGeometry, InDeltaTime)
+
+-- end
+
+-- function wq2:Destruct()
+
+-- end
+
+return wq2
