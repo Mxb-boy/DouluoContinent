74773ab636da75a3fef316540ab6d183551f06c3
陈思妍 <3231283647@qq.com>
2026-08-15 12:11:36 +0800

    wq2 ui017 优化

--- Script/Blueprint/UI/wq2.lua
+++ Script/Blueprint/UI/wq2.lua
@@ -2,15 +2,13 @@
 ---@field Button_148 UButton
 ---@field Button_149 UButton
 ---@field Button_261 UButton
+---@field Image_1 UImage
 ---@field Image_16 UImage
 ---@field Image_17 UImage
 ---@field Image_18 UImage
 ---@field Image_154 UImage
 ---@field Image_155 UImage
 ---@field Image_296 UImage
----@field Image_405 UImage
----@field Image_406 UImage
----@field Image_407 UImage
 ---@field Image_408 UImage
 --Edit Below--
 local wq2 = { bInitDoOnce = false }
