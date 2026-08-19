ac628d840e37e85f86ce387502468ffee297d204
陈思妍 <3231283647@qq.com>
2026-08-19 17:59:59 +0800

    礼包

---
+++ Script/Blueprint/Yan/UI/lb1.lua
@@ -0,0 +1,32 @@
+---@class lb1_C:UUserWidget
+---@field Button_39 UButton
+---@field Image_56 UImage
+---@field Image_145 UImage
+---@field Image_146 UImage
+---@field Image_147 UImage
+---@field Image_148 UImage
+---@field Image_149 UImage
+---@field Image_150 UImage
+---@field Image_151 UImage
+---@field Image_152 UImage
+---@field Image_180 UImage
+---@field Image_248 UImage
+---@field Image_249 UImage
+--Edit Below--
+local lb1 = { bInitDoOnce = false } 
+
+--[==[ Construct
+function lb1:Construct()
+	
+end
+-- Construct ]==]
+
+-- function lb1:Tick(MyGeometry, InDeltaTime)
+
+-- end
+
+-- function lb1:Destruct()
+
+-- end
+
+return lb1
---
+++ Script/Blueprint/Yan/UI/shouchong1.lua
@@ -0,0 +1,88 @@
+---@class shouchong1_C:UUserWidget
+---@field Btn_Close UButton
+---@field Button_7 UButton
+---@field Button_8 UButton
+---@field Button_9 UButton
+---@field Button_10 UButton
+---@field Button_39 UButton
+---@field Image_10 UImage
+---@field Image_35 UImage
+---@field Image_52 UImage
+---@field Image_56 UImage
+---@field Image_145 UImage
+---@field Image_146 UImage
+---@field Image_147 UImage
+---@field Image_148 UImage
+---@field Image_149 UImage
+---@field Image_150 UImage
+---@field Image_151 UImage
+---@field Image_152 UImage
+---@field Image_180 UImage
+---@field Image_248 UImage
+---@field Image_249 UImage
+---@field Image_301 UImage
+---@field Image_302 UImage
+---@field Image_303 UImage
+---@field Image_304 UImage
+---@field Image_305 UImage
+---@field Image_306 UImage
+---@field Image_307 UImage
+---@field Image_308 UImage
+---@field Image_309 UImage
+---@field Image_310 UImage
+---@field Image_311 UImage
+---@field Image_312 UImage
+---@field Image_313 UImage
+---@field Image_314 UImage
+---@field Image_315 UImage
+---@field Image_316 UImage
+---@field Image_317 UImage
+---@field Image_318 UImage
+---@field Image_319 UImage
+---@field Image_320 UImage
+---@field Image_321 UImage
+---@field Image_322 UImage
+---@field Image_323 UImage
+---@field Image_324 UImage
+---@field Image_325 UImage
+---@field Image_326 UImage
+---@field Image_327 UImage
+---@field Image_328 UImage
+---@field Image_329 UImage
+---@field Image_330 UImage
+---@field Image_331 UImage
+---@field Image_332 UImage
+---@field Image_333 UImage
+---@field Image_334 UImage
+---@field Image_335 UImage
+---@field Image_336 UImage
+---@field Image_337 UImage
+---@field Image_338 UImage
+---@field Image_339 UImage
+---@field Image_340 UImage
+---@field Image_341 UImage
+---@field Image_342 UImage
+---@field Image_343 UImage
+---@field Image_344 UImage
+---@field Image_345 UImage
+---@field Image_346 UImage
+---@field Image_347 UImage
+---@field Image_348 UImage
+--Edit Below--
+local shouchong1 = { bInitDoOnce = false } 
+
+--[==[ Construct
+function shouchong1:Construct()
+	
+end
+-- Construct ]==]
+
+-- function shouchong1:Tick(MyGeometry, InDeltaTime)
+
+-- end
+
+-- function shouchong1:Destruct()
+
+-- end
+
+return shouchong1
